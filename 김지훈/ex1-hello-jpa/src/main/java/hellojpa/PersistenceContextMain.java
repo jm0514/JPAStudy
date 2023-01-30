d3585091486e617e7306c575f38511059f036a3b
Kim Ji Hun <gmldnr2222@naver.com>
2023-01-30 17:03:47 +0900

    feat: add PersistenceContextMain, managed status

---
+++ 김지훈/ex1-hello-jpa/src/main/java/hellojpa/PersistenceContextMain.java
@@ -0,0 +1,36 @@
+package hellojpa;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+
+public class PersistenceContextMain {
+
+	public static void main(String[] args) {
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+		EntityManager em = emf.createEntityManager();
+		EntityTransaction tx = em.getTransaction();
+		tx.begin();
+
+		try {
+			// 엔티티를 생성한 상태 (비영속)
+			Member member = new Member();
+			member.setId(100L);
+			member.setName("HelloJPA");
+
+			// 엔티티를 영속 (DB에 저장되지 않는다. 쿼리가 여기서 나오지 않는다.)
+			System.out.println("=== BEFORE ===");
+			em.persist(member);
+			System.out.println("=== AFTER ===");
+
+			tx.commit();
+		} catch (Exception e) {
+			tx.rollback();
+		} finally {
+			em.close();
+		}
+
+		emf.close();
+	}
+}
