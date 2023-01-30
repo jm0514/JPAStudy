24fabf15ab6a261d8aefe87fd9a1a500462903dd
Kim Ji Hun <gmldnr2222@naver.com>
2023-01-30 14:40:45 +0900

    feat: JpaMain JPA Intro

--- 김지훈/ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
+++ 김지훈/ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
@@ -1,17 +1,51 @@
 package hellojpa;
 
+import java.util.List;
+
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
 
 public class JpaMain {
 	public static void main(String[] args) {
 		EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
 		EntityManager em = emf.createEntityManager();
+		EntityTransaction tx = em.getTransaction();
+		tx.begin();
 
-		// code
+		try {
+			// 회원 등록
+			/*Member member = new Member();
+			member.setId(1L);
+			member.setName("HelloA");
+			member.setId(2L);
+			member.setName("HelloB");
+			em.persist(member);*/
 
-		em.close();
+			// 회원 조회 및 수정
+			/*Member findMember = em.find(Member.class, 1L);
+			// System.out.println(findMember);
+			findMember.setName("HelloJPA");*/
+
+			// 회원 삭제
+			/*Member findMember = em.find(Member.class, 1L);
+			em.remove(findMember);*/
+
+			// JPQL
+			List<Member> result = em.createQuery("select m from Member m", Member.class)
+				.setFirstResult(0)
+				.setMaxResults(10)
+				.getResultList();
+			System.out.println(result);
+
+			tx.commit();
+		} catch (Exception e) {
+			tx.rollback();
+		} finally {
+			em.close();
+		}
+
 		emf.close();
 	}
 }
