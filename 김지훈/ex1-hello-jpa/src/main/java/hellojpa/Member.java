57b1b234ff70542e21f53facad156af5c7bbfccf
Kim Ji Hun <gmldnr2222@naver.com>
2023-01-30 14:40:31 +0900

    feat: Member Entity

---
+++ 김지훈/ex1-hello-jpa/src/main/java/hellojpa/Member.java
@@ -0,0 +1,18 @@
+package hellojpa;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Entity
+@Getter
+@Setter
+@ToString
+public class Member {
+	@Id
+	private Long id;
+	private String name;
+}
