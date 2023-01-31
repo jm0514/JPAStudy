6850b12881891527aea2487aa8ee1d24d9fe8bdf
Kim Ji Hun <gmldnr2222@naver.com>
2023-01-31 14:41:16 +0900

    feat: 1차 캐시에서 조회

--- 김지훈/ex1-hello-jpa/src/main/java/hellojpa/PersistenceContextMain.java
+++ 김지훈/ex1-hello-jpa/src/main/java/hellojpa/PersistenceContextMain.java
@@ -16,13 +16,16 @@ public class PersistenceContextMain {
 		try {
 			// 엔티티를 생성한 상태 (비영속)
 			Member member = new Member();
-			member.setId(100L);
+			member.setId(101L);
 			member.setName("HelloJPA");
 
 			// 엔티티를 영속 (DB에 저장되지 않는다. 쿼리가 여기서 나오지 않는다.)
 			System.out.println("=== BEFORE ===");
 			em.persist(member);
 			System.out.println("=== AFTER ===");
+
+			Member findMember = em.find(Member.class, 101L); // 1차 캐시에서 조회.
+			System.out.println(findMember);
 
 			tx.commit();
 		} catch (Exception e) {
