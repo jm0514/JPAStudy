035f0aad1f70bc900dc5fe5389c78680cfbd45a8
Kim Ji Hun <gmldnr2222@naver.com>
2023-01-31 15:03:57 +0900

    feat: add Constructor in Member

--- 김지훈/ex1-hello-jpa/src/main/java/hellojpa/Member.java
+++ 김지훈/ex1-hello-jpa/src/main/java/hellojpa/Member.java
@@ -3,7 +3,9 @@ package hellojpa;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
 
@@ -11,6 +13,8 @@ import lombok.ToString;
 @Getter
 @Setter
 @ToString
+@NoArgsConstructor
+@AllArgsConstructor
 public class Member {
 	@Id
 	private Long id;
