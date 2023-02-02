65509f626bc1dda1a4bb95ada7832df9c7a3dd5a
Kim Ji Hun <gmldnr2222@naver.com>
2023-02-02 14:06:09 +0900

    feat: Member Entity field-column mapping

--- 김지훈/ex1-hello-jpa/src/main/java/hellojpa/Member.java
+++ 김지훈/ex1-hello-jpa/src/main/java/hellojpa/Member.java
@@ -1,7 +1,17 @@
 package hellojpa;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.Date;
+
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
 import javax.persistence.Id;
+import javax.persistence.Lob;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -18,5 +28,29 @@ import lombok.ToString;
 public class Member {
 	@Id
 	private Long id;
-	private String name;
+
+	@Column(name = "name")
+	private String username;
+
+	private Integer age;
+
+	@Enumerated(EnumType.STRING)
+	private RoleType roleType;
+
+	@Temporal(TemporalType.TIMESTAMP)
+	private Date createdDate;
+
+	@Temporal(TemporalType.TIMESTAMP)
+	private Date lastModifiedDate;
+
+	private LocalDate testLocalDate;
+	private LocalDateTime testLocalDateTime;
+
+	@Lob
+	private String description;
+
+	public Member(Long id, String username) {
+		this.id = id;
+		this.username = username;
+	}
 }
