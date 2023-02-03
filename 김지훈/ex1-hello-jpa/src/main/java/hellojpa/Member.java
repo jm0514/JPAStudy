82bdb7069e296d1fc3dafd8350638d47e0b09ad3
Kim Ji Hun <gmldnr2222@naver.com>
2023-02-03 14:01:24 +0900

    feat: change simply Member Entity

--- 김지훈/ex1-hello-jpa/src/main/java/hellojpa/Member.java
+++ 김지훈/ex1-hello-jpa/src/main/java/hellojpa/Member.java
@@ -1,19 +1,10 @@
 package hellojpa;
 
-import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.util.Date;
-
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.EnumType;
-import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.Lob;
-import javax.persistence.Table;
-import javax.persistence.Temporal;
-import javax.persistence.TemporalType;
-import javax.persistence.UniqueConstraint;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -27,10 +18,6 @@ import lombok.ToString;
 @ToString
 @NoArgsConstructor
 @AllArgsConstructor
-@Table(name = "MEMBER", uniqueConstraints = {@UniqueConstraint(
-	name = "NAME_AGE_UNIQUE",
-	columnNames = {"NAME", "AGE"}
-)})
 public class Member {
 	@Id
 	private Long id;
@@ -38,25 +25,4 @@ public class Member {
 	@Column(name = "name", nullable = false, length = 10)
 	private String username;
 
-	private Integer age;
-
-	@Enumerated(EnumType.STRING)
-	private RoleType roleType;
-
-	@Temporal(TemporalType.TIMESTAMP)
-	private Date createdDate;
-
-	@Temporal(TemporalType.TIMESTAMP)
-	private Date lastModifiedDate;
-
-	private LocalDate testLocalDate;
-	private LocalDateTime testLocalDateTime;
-
-	@Lob
-	private String description;
-
-	public Member(Long id, String username) {
-		this.id = id;
-		this.username = username;
-	}
 }
