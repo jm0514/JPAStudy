ec75386a8c1b67faa161c151d77318e94041ae6a
Kim Ji Hun <gmldnr2222@naver.com>
2023-02-07 21:56:49 +0900

    feat: 실전 예제 3 - 다양한 연관관계 매핑

--- 김지훈/jpashop/src/main/java/jpabook/jpashop/domain/Order.java
+++ 김지훈/jpashop/src/main/java/jpabook/jpashop/domain/Order.java
@@ -13,6 +13,7 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
 import lombok.Getter;
@@ -32,6 +33,10 @@ public class Order {
 	@ManyToOne
 	@JoinColumn(name = "MEMBER_ID")
 	private Member member;
+
+	@OneToOne
+	@JoinColumn(name = "DELIVERY_ID")
+	private Delivery delivery;
 
 	@OneToMany(mappedBy = "order")
 	private List<OrderItem> orderItems = new ArrayList<>();
@@ -53,5 +58,10 @@ public class Order {
 	public void addOrderItem(OrderItem orderItem) {
 		orderItems.add(orderItem);
 		orderItem.setOrder(this);
+	}
+
+	public void setDelivery(Delivery delivery) {
+		this.delivery = delivery;
+		delivery.setOrder(this);
 	}
 }
