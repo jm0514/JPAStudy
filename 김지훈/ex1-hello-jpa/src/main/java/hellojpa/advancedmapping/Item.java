5bd5d25225a07604f697606532678d8fed2ac657
Kim Ji Hun <gmldnr2222@naver.com>
2023-02-07 22:42:48 +0900

    feat: 상속관계 매핑 -  조인 전략 (InheritanceType.JOINED)

--- 김지훈/ex1-hello-jpa/src/main/java/hellojpa/advancedmapping/Item.java
+++ 김지훈/ex1-hello-jpa/src/main/java/hellojpa/advancedmapping/Item.java
@@ -3,12 +3,16 @@ package hellojpa.advancedmapping;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.Inheritance;
+import javax.persistence.InheritanceType;
+
 import lombok.Getter;
 import lombok.Setter;
 
 @Entity
 @Getter
 @Setter
+@Inheritance(strategy = InheritanceType.JOINED)
 public class Item {
 
 	@Id
