0c721d6e7decb6fc37ec5488a726d3f4a330af48
Kim Ji Hun <gmldnr2222@naver.com>
2023-02-07 22:24:32 +0900

    feat: add Item, Album, Book, Movie Entity for Inheritance mapping

---
+++ 김지훈/ex1-hello-jpa/src/main/java/hellojpa/advancedmapping/Item.java
@@ -0,0 +1,17 @@
+package hellojpa.advancedmapping;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class Item {
+
+	@Id
+	@GeneratedValue
+	private Long id;
+
+	private String name;
+	private int price;
+
+}
