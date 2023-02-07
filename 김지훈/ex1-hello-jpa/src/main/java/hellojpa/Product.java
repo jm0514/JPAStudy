a86b2a9badf4a6d582b16b9954cf1a3d18af1dad
Kim Ji Hun <gmldnr2222@naver.com>
2023-02-07 15:38:36 +0900

    feat: 다대다 양방향 (Member, Product)

--- 김지훈/ex1-hello-jpa/src/main/java/hellojpa/Product.java
+++ 김지훈/ex1-hello-jpa/src/main/java/hellojpa/Product.java
@@ -1,8 +1,12 @@
 package hellojpa;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.ManyToMany;
 
 import lombok.Getter;
 import lombok.Setter;
@@ -16,4 +20,7 @@ public class Product {
 	private Long id;
 
 	private String name;
+
+	@ManyToMany(mappedBy = "products")
+	private List<Member> members = new ArrayList<>();
 }
