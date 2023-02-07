1e700789f93e44ba39b3f4bf702fa9214a92a9b3
Kim Ji Hun <gmldnr2222@naver.com>
2023-02-07 13:00:34 +0900

    feat: setTeam() in Member entity

--- 김지훈/ex1-hello-jpa/src/main/java/hellojpa/Member.java
+++ 김지훈/ex1-hello-jpa/src/main/java/hellojpa/Member.java
@@ -28,10 +28,16 @@ public class Member {
 	@Column(name = "USERNAME")
 	private String username;
 
-	// @Column(name = "TEAM_ID")
-	// private Long teamId;
-
 	@ManyToOne
 	@JoinColumn(name = "TEAM_ID")
 	private Team team;
+
+	public void setTeam(Team team) {
+		this.team = team;
+
+		// 무한루프에 빠지지 않도록 체크
+		if (!team.getMembers().contains(this)) {
+			team.getMembers().add(this);
+		}
+	}
 }
