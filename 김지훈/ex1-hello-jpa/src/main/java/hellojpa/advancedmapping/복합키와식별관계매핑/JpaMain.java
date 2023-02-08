8c5e8f04502956aaabbb4a1819a68a4034e01997
Kim Ji Hun <gmldnr2222@naver.com>
2023-02-08 14:33:43 +0900

    feat: 복합 키 이용해 저장, 조회

---
+++ 김지훈/ex1-hello-jpa/src/main/java/hellojpa/advancedmapping/복합키와식별관계매핑/JpaMain.java
@@ -0,0 +1,41 @@
+package hellojpa.advancedmapping.복합키와식별관계매핑;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+
+public class JpaMain {
+	public static void main(String[] args) {
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+		EntityManager em = emf.createEntityManager();
+		EntityTransaction tx = em.getTransaction();
+		tx.begin();
+
+		try {
+
+			// 저장
+			Parent parent = new Parent();
+			parent.setId1("id1");
+			parent.setId2("id2");
+			parent.setName("parentName");
+			em.persist(parent);
+
+			em.flush();
+			em.clear();
+
+			// 조회
+			ParentId parentId = new ParentId("id1", "id2");
+			Parent findParent = em.find(Parent.class, parentId);
+			System.out.println(findParent);
+
+			tx.commit();
+		} catch (Exception e) {
+			tx.rollback();
+		} finally {
+			em.close();
+		}
+
+		emf.close();
+	}
+}
