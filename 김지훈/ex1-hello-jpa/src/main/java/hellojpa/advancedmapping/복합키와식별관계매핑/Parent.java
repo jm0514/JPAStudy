7c00bf69bd29752bf74c018abae8ee43df4e48b4
Kim Ji Hun <gmldnr2222@naver.com>
2023-02-08 14:32:26 +0900

    feat: 복합키와 식별관계 매핑 - Parent, ParentId

---
+++ 김지훈/ex1-hello-jpa/src/main/java/hellojpa/advancedmapping/복합키와식별관계매핑/Parent.java
@@ -0,0 +1,28 @@
+package hellojpa.advancedmapping.복합키와식별관계매핑;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.IdClass;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Entity
+@IdClass(ParentId.class)
+@Getter
+@Setter
+@ToString
+public class Parent {
+
+	@Id
+	@Column(name = "PARENT_ID1")
+	private String id1; // Parent.id1 매핑
+
+	@Id
+	@Column(name = "PARENT_ID2")
+	private String id2; // Parent.id2 매핑
+
+	private String name;
+}
