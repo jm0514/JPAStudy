c34d3cdbe52acf5c0014a21e707f784b8b52f9a0
Kim Ji Hun <gmldnr2222@naver.com>
2023-02-13 19:24:10 +0900

    feat: start criteria

--- 김지훈/ex1-hello-jpa/src/main/java/hellojpa/valuetype/JpaMain.java
+++ 김지훈/ex1-hello-jpa/src/main/java/hellojpa/valuetype/JpaMain.java
@@ -8,6 +8,9 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
 
 public class JpaMain {
 	public static void main(String[] args) {
@@ -86,15 +89,27 @@ public class JpaMain {
 			findMember.getAddressHistory().add(new AddressEntity("newCity1", "street", "123456"));*/
 
 			// jpql
-			List<Member> resultList = em.createQuery(
+			/*List<Member> resultList = em.createQuery(
 				"select m from Member m where m.username like '%mem%'",
 				Member.class
 			).getResultList();
 
 			for (Member result : resultList) {
 				System.out.println("member = " + result.getUsername());
-			}
+			}*/
 
+			CriteriaBuilder cb = em.getCriteriaBuilder();
+			CriteriaQuery<Member> query = cb.createQuery(Member.class);
+
+			Root<Member> m = query.from(Member.class);
+
+			CriteriaQuery<Member> cq = query.select(m).where(cb.equal(m.get("username"), "member1"));
+			List<Member> resultList = em.createQuery(cq)
+										.getResultList();
+
+			for (Member member1 : resultList) {
+				System.out.println("member = " + member1.getUsername());
+			}
 			tx.commit();
 		} catch (Exception e) {
 			e.printStackTrace();
