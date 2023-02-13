20e75ec530e25abe205a6ce9efcb213e1a0be779
Kim Ji Hun <gmldnr2222@naver.com>
2023-02-13 16:47:13 +0900

    feat: 값 타입 컬렉션 저장 예제

--- 김지훈/ex1-hello-jpa/src/main/java/hellojpa/valuetype/JpaMain.java
+++ 김지훈/ex1-hello-jpa/src/main/java/hellojpa/valuetype/JpaMain.java
@@ -24,7 +24,7 @@ public class JpaMain {
 
 			// side effect
 			// member1.address 의 city만 변경했음에도, 값을 공유해 member2.address의 city도 같이 변경됨.
-			Address address = new Address("city", "street", "123456");
+			/*Address address = new Address("city", "street", "123456");
 			Member member1 = new Member("member1", new Period(LocalDateTime.now(), LocalDateTime.now()), address);
 			em.persist(member1);
 
@@ -35,7 +35,20 @@ public class JpaMain {
 			em.persist(member2);
 
 			member1.getHomeAddress().setCity("newCity");
-			em.persist(member1);
+			em.persist(member1);*/
+
+			Member member = new Member();
+			member.setUsername("member1");
+			member.setHomeAddress(new Address("homeCity", "street", "123456"));
+
+			member.getFavoriteFoods().add("치킨");
+			member.getFavoriteFoods().add("피자");
+			member.getFavoriteFoods().add("족발");
+
+			member.getAddressHistory().add(new Address("old1", "street", "123456"));
+			member.getAddressHistory().add(new Address("old2", "street", "123456"));
+
+			em.persist(member);
 
 			tx.commit();
 		} catch (Exception e) {
