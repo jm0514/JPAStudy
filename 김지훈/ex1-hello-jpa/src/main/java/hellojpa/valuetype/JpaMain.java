a9233ed725551afe4e58d94a12a50cd37feb2567
Kim Ji Hun <gmldnr2222@naver.com>
2023-02-13 16:50:15 +0900

    feat: 값 타입 조회 예제 - 값 타입 컬렉션도 지연 로딩 전략 사용

--- 김지훈/ex1-hello-jpa/src/main/java/hellojpa/valuetype/JpaMain.java
+++ 김지훈/ex1-hello-jpa/src/main/java/hellojpa/valuetype/JpaMain.java
@@ -1,6 +1,8 @@
 package hellojpa.valuetype;
 
 import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Set;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
@@ -49,6 +51,22 @@ public class JpaMain {
 			member.getAddressHistory().add(new Address("old2", "street", "123456"));
 
 			em.persist(member);
+
+			em.flush();
+			em.clear();
+
+			System.out.println("=============== START ===============");
+			Member findMember = em.find(Member.class, member.getId());
+
+			List<Address> addressHistory = findMember.getAddressHistory(); // 지연 로딩
+			for (Address address : addressHistory) {
+				System.out.println("address = " + address);
+			}
+
+			Set<String> favoriteFoods = findMember.getFavoriteFoods();
+			for (String favoriteFood : favoriteFoods) {
+				System.out.println("favoriteFood = " + favoriteFood);
+			}
 
 			tx.commit();
 		} catch (Exception e) {
