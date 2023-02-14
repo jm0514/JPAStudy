6be8b4ef33673f309d368d0d6d4fb89188edc182
Kim Ji Hun <gmldnr2222@naver.com>
2023-02-14 14:55:44 +0900

    feat: Projection (SELECT)

--- 김지훈/hellojpa/src/main/java/jpql/JpaMain.java
+++ 김지훈/hellojpa/src/main/java/jpql/JpaMain.java
@@ -34,11 +34,45 @@ public class JpaMain {
 				Member.class);
 			query.setParameter("username", "member1");
 			Member singleResult = query.getSingleResult();*/
-			Member singleResult = em.createQuery("select m from Member m where m.username = :username",
+			/*Member singleResult = em.createQuery("select m from Member m where m.username = :username",
 				Member.class)
 				.setParameter("username", "member1")
 				.getSingleResult();
-			System.out.println("singleResult = " + singleResult.getUsername());
+			System.out.println("singleResult = " + singleResult.getUsername());*/
+
+			em.flush();
+			em.clear();
+
+			/*List<Member> result = em.createQuery("select m from Member m", Member.class)
+				.getResultList(); // 엔티티 프로젝션
+
+			Member findMember = result.get(0);
+			findMember.setAge(20); // 영속성 컨텍스트에 의해 관리 됨.*/
+			/*List<Team> resultList = em.createQuery("select m.team from Member m", Team.class)
+				.getResultList();*/ // join 발생 아래와 같이 변경해 주는 것이 명시적으로 좋다.
+			/*List<Team> resultList = em.createQuery("select t from Member m join m.team t", Team.class)
+				.getResultList();*/
+
+			/*em.createQuery("select o.address from Order o", Address.class)
+				.getResultList();*/ // 임베디드 타입 프로젝션
+			/*List resultList = em.createQuery("select m.username, m.age from Member m")
+				.getResultList();
+
+			Object o = resultList.get(0);
+			Object[] result = (Object[]) o;*/
+			/*List<Object[]> resultList = em.createQuery("select m.username, m.age from Member m")
+				.getResultList();
+
+			Object[] result = resultList.get(0);
+			System.out.println("username = " + result[0]);
+			System.out.println("age = " + result[1]);*/
+
+			List<MemberDTO> result = em.createQuery("select new jpql.MemberDTO(m.username, m.age) from Member m",
+					MemberDTO.class)
+				.getResultList();
+			MemberDTO memberDTO = result.get(0);
+			System.out.println("memberDTO.getUsername() = " + memberDTO.getUsername());
+			System.out.println("memberDTO.getAge() = " + memberDTO.getAge());
 
 			tx.commit();
 		} catch (Exception e) {
