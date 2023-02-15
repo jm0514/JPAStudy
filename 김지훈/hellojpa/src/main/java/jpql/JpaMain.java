aa3e70b47755701776fe4eb5ba57d349fbfb50c4
Kim Ji Hun <gmldnr2222@naver.com>
2023-02-15 10:46:42 +0900

    feat: 조건식 (CASE, COALESCE, NULLIF ...)

--- 김지훈/hellojpa/src/main/java/jpql/JpaMain.java
+++ 김지훈/hellojpa/src/main/java/jpql/JpaMain.java
@@ -90,7 +90,8 @@ public class JpaMain {
 			em.persist(team);
 
 			Member member = new Member();
-			member.setUsername("member1");
+			// member.setUsername(null);
+			member.setUsername("관리자");
 			member.setAge(10);
 			member.setType(MemberType.ADMIN);
 			member.changeTeam(team);
@@ -110,7 +111,7 @@ public class JpaMain {
 				.getResultList();*/
 
 			// JPQL 타입 표현과 기타식
-			String query = "select m.username, 'HELLO', TRUE from Member m"
+			/*String query = "select m.username, 'HELLO', TRUE from Member m"
 				+ "    where m.type = :userType";
 			List<Object[]> result = em.createQuery(query)
 							.setParameter("userType", MemberType.ADMIN)
@@ -119,7 +120,30 @@ public class JpaMain {
 				System.out.println("objects[0] = " + objects[0]);
 				System.out.println("objects[1] = " + objects[1]);
 				System.out.println("objects[2] = " + objects[2]);
+			}*/
+
+			// 기본 CASE 식
+			/*String query =
+						"select "
+							+ "case when m.age <= 10 then '학생요금'"
+							+ " 	when m.age >= 60 then '경로요금'"
+							+ "		else '일반요금' end "
+						+ "from Member m";*/
+
+			// COALESCE
+			/*String query =
+						"select coalesce(m.username, '이름 없는 회원') as username from Member m";*/
+			String query =
+						"select nullif(m.username, '관리자') from Member m";
+
+			List<String> result = em.createQuery(query, String.class)
+				.getResultList();
+
+			for (String s : result) {
+				System.out.println("s = " + s);
 			}
+
+
 
 			tx.commit();
 		} catch (Exception e) {
