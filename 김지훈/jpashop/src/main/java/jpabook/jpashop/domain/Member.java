7d6976369ac6b61e390214958d92ff3dfb4715c5
Kim Ji Hun <gmldnr2222@naver.com>
2023-02-25 17:06:17 +0900

    feat: add @NotEmpty name in Member

--- 김지훈/jpashop/src/main/java/jpabook/jpashop/domain/Member.java
+++ 김지훈/jpashop/src/main/java/jpabook/jpashop/domain/Member.java
@@ -9,6 +9,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
+import jakarta.validation.constraints.NotEmpty;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -22,6 +23,7 @@ public class Member {
 	@Column(name = "member_id")
 	private Long id;
 
+	@NotEmpty
 	private String name;
 
 	@Embedded
