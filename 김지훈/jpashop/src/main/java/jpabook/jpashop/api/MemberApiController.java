b77c2e3b7bdbd0da307caab68f3f3809246ca338
Kim Ji Hun <gmldnr2222@naver.com>
2023-02-25 17:47:55 +0900

    feat: @GetMapping("/api/v2/members") membersV2 + Result, MemberDto

--- 김지훈/jpashop/src/main/java/jpabook/jpashop/api/MemberApiController.java
+++ 김지훈/jpashop/src/main/java/jpabook/jpashop/api/MemberApiController.java
@@ -1,6 +1,7 @@
 package jpabook.jpashop.api;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -26,6 +27,29 @@ public class MemberApiController {
 	@GetMapping("/api/v1/members")
 	public List<Member> membersV1() {
 		return memberService.findMembers();
+	}
+
+	@GetMapping("/api/v2/members")
+	public Result membersV2() {
+		List<Member> findMembers = memberService.findMembers();
+		List<MemberDto> memberDtos = findMembers.stream()
+			.map(m -> new MemberDto(m.getName()))
+			.collect(Collectors.toList());
+
+		return new Result(memberDtos.size(), memberDtos);
+	}
+
+	@Data
+	@AllArgsConstructor
+	static class Result<T> {
+		private int count;
+		private T data;
+	}
+
+	@Data
+	@AllArgsConstructor
+	static class MemberDto {
+		private String name;
 	}
 
 	@PostMapping("/api/v1/members")
