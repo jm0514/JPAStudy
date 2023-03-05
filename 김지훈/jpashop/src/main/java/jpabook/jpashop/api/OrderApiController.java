b665cb4b45d0a9a46405a347efb955519c18c541
Kim Ji Hun <gmldnr2222@naver.com>
2023-03-05 17:38:42 +0900

    feat: ordersV3_page() in OrderApiController
    - findAllWithMemberDelivery() in OrderRepository
    - jpa.properties.default_batch_fetch_size in application.yml

--- 김지훈/jpashop/src/main/java/jpabook/jpashop/api/OrderApiController.java
+++ 김지훈/jpashop/src/main/java/jpabook/jpashop/api/OrderApiController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import jpabook.jpashop.domain.Address;
@@ -48,9 +49,19 @@ public class OrderApiController {
 	public List<OrderDto> ordersV3() {
 		List<Order> orders = orderRepository.findAllWithItem();
 
-		for (Order order : orders) {
-			System.out.println("order = " + order + " id=" + order.getId());
-		}
+		List<OrderDto> result = orders.stream()
+			.map(o -> new OrderDto(o))
+			.collect(Collectors.toList());
+
+		return result;
+	}
+
+	@GetMapping("/api/v3.1/orders")
+	public List<OrderDto> ordersV3_page(
+		@RequestParam(value = "offset", defaultValue = "0") int offset,
+		@RequestParam(value = "limit", defaultValue = "100") int limit
+	) {
+		List<Order> orders = orderRepository.findAllWithMemberDelivery(offset, limit);
 
 		List<OrderDto> result = orders.stream()
 			.map(o -> new OrderDto(o))
--- 김지훈/jpashop/src/main/java/jpabook/jpashop/repository/OrderRepository.java
+++ 김지훈/jpashop/src/main/java/jpabook/jpashop/repository/OrderRepository.java
@@ -102,9 +102,19 @@ public class OrderRepository {
 	public List<Order> findAllWithMemberDelivery() {
 		return em.createQuery(
 			"select o from Order o"
-					+ "	join fetch o.member m"
-					+ " join fetch o.delivery d", Order.class
+				+ "	join fetch o.member m"
+				+ " join fetch o.delivery d", Order.class
 		).getResultList();
+	}
+
+	public List<Order> findAllWithMemberDelivery(int offset, int limit) {
+		return em.createQuery(
+			"select o from Order o"
+				+ "	join fetch o.member m"
+				+ " join fetch o.delivery d", Order.class)
+			.setFirstResult(offset)
+			.setMaxResults(limit)
+			.getResultList();
 	}
 
 	public List<Order> findAllWithItem() {
