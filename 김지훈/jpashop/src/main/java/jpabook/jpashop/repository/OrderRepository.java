0b864c8d79ee4a6b0faa0912a04b45ec8814f9c4
Kim Ji Hun <gmldnr2222@naver.com>
2023-03-08 12:42:46 +0900

    feat: start querydsl in spring boot 3.0

--- 김지훈/jpashop/src/main/java/jpabook/jpashop/repository/OrderRepository.java
+++ 김지훈/jpashop/src/main/java/jpabook/jpashop/repository/OrderRepository.java
@@ -6,6 +6,9 @@ import java.util.List;
 import org.springframework.stereotype.Repository;
 import org.springframework.util.StringUtils;
 
+import com.querydsl.core.types.dsl.BooleanExpression;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.TypedQuery;
 import jakarta.persistence.criteria.CriteriaBuilder;
@@ -15,6 +18,9 @@ import jakarta.persistence.criteria.JoinType;
 import jakarta.persistence.criteria.Predicate;
 import jakarta.persistence.criteria.Root;
 import jpabook.jpashop.domain.Order;
+import jpabook.jpashop.domain.OrderStatus;
+import jpabook.jpashop.domain.QMember;
+import jpabook.jpashop.domain.QOrder;
 import lombok.RequiredArgsConstructor;
 
 @Repository
@@ -97,6 +103,29 @@ public class OrderRepository {
 		cq.where(cb.and(criteria.toArray(new Predicate[criteria.size()])));
 		TypedQuery<Order> query = em.createQuery(cq).setMaxResults(1000);
 		return query.getResultList();
+	}
+
+	public List<Order> findAll(OrderSearch orderSearch) {
+		JPAQueryFactory query = new JPAQueryFactory(em);
+		QOrder order = QOrder.order;
+		QMember member = QMember.member;
+		return query
+			.select(order)
+			.from(order)
+			.join(order.member, member)
+			.where(statusEq(orderSearch.getOrderStatus()),  nameLike(orderSearch.getMemberName()))
+			.limit(1000)
+			.fetch();
+	}
+
+	private BooleanExpression nameLike(String memberName) {
+		if (!StringUtils.hasText(memberName)) return null;
+		return QMember.member.name.like(memberName);
+	}
+
+	private BooleanExpression statusEq(OrderStatus statusCond) {
+		if (statusCond == null) return null;
+		return QOrder.order.status.eq(statusCond);
 	}
 
 	public List<Order> findAllWithMemberDelivery() {
