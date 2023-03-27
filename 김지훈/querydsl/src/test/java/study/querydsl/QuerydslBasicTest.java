f1f2a2c3bea41edb58c5fc7ac65f3b8d14766901
Kim Ji Hun <gmldnr2222@naver.com>
2023-03-27 14:54:08 +0900

    feat: orderBy에서 Case 문 함께 사용하기 예제

--- 김지훈/querydsl/src/test/java/study/querydsl/QuerydslBasicTest.java
+++ 김지훈/querydsl/src/test/java/study/querydsl/QuerydslBasicTest.java
@@ -16,6 +16,7 @@ import org.springframework.transaction.annotation.Transactional;
 import com.querydsl.core.QueryResults;
 import com.querydsl.core.Tuple;
 import com.querydsl.core.types.dsl.CaseBuilder;
+import com.querydsl.core.types.dsl.NumberExpression;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 
 import jakarta.persistence.EntityManager;
@@ -442,6 +443,30 @@ public class QuerydslBasicTest {
 
 		for (String s : result) {
 			System.out.println("s = " + s);
+		}
+	}
+
+	/**
+	 * orderBy 에서 Case 문 함께 사용하기 예제
+	 */
+	@Test
+	void orderByCase() {
+		NumberExpression<Integer> rankPath = new CaseBuilder()
+			.when(member.age.between(0, 20)).then(2)
+			.when(member.age.between(21, 30)).then(1)
+			.otherwise(3);
+
+		List<Tuple> result = queryFactory
+			.select(member.username, member.age, rankPath)
+			.from(member)
+			.orderBy(rankPath.desc())
+			.fetch();
+
+		for (Tuple tuple : result) {
+			String username = tuple.get(member.username);
+			Integer age = tuple.get(member.age);
+			Integer rank = tuple.get(rankPath);
+			System.out.println("username : " + username + " age : " + age + " rank : "+ rank);
 		}
 	}
 }
